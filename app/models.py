09f2a5f408fd289266aa70951fedfd1ba945694e
Andrew <drew.forall@gmail.com>
2020-12-01 16:32:18 +0300

    [ADD] Added exist models to API

--- app/models.py
+++ app/models.py
@@ -3,11 +3,20 @@ from django.db import models
 class Currency(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False)
 
+    def __str__(self):
+        return self.name
+
 class Item(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False)
     code = models.CharField(max_length=255, unique=True, blank=False)
     currency = models.ForeignKey("Currency", on_delete=models.CASCADE, blank=False)
 
+    def __str__(self):
+        return self.code
+
 class Price(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
     item = models.ForeignKey("Item", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item.code + " " + str(self.price) + " " + self.item.currency.name
