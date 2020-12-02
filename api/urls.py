5b7c9f8ea845e44b013d40302659953ad4ed9860
Andrew <drew.forall@gmail.com>
2020-12-02 13:09:50 +0300

    [ADD] jwt auth

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+
 from .views import CurrencyViewSet, PriceViewSet, ItemViewSet, UserView
+
 
 router = routers.DefaultRouter()
 router.register(r'currencies', CurrencyViewSet, basename="Currencies")
@@ -10,4 +13,7 @@ router.register(r'reg', UserView, basename="reg")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', obtain_jwt_token),
+    path('token/verify/', verify_jwt_token),
+    path('token/refresh/', refresh_jwt_token),
 ]
