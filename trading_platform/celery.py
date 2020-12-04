561ce776167f738504b06458ff3c365cc99d3308
Andrew <drew.forall@gmail.com>
2020-12-04 13:00:54 +0300

    [ADD] celery, working test task

--- trading_platform/celery.py
+++ trading_platform/celery.py
@@ -2,6 +2,11 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trading_platform.settings')
-celery_app = Celery('trading_platform')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
+app = Celery('trading_platform', broker='redis://redis:6379/0');
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
